4b3fd4f27867c74bd7fcd4b014a3265e94482ee8
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-17 11:11:51 -0300

    adicionando a logica

--- lm35.ino
+++ lm35.ino
@@ -1,9 +1,13 @@
-
+const int LM35 = A0;
 
 void setup() {
-
+  pinMode(LM35, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  float valor = analogRead(LM35);
+  Serial.println("Temperatura: ");
+  Serial.println((valor * 5) / 1023 / 0.01);
+  delay(1000);
 }
